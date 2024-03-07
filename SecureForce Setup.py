00e773e40ff4906d7c977bd37b7a011723233190
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2024-03-07 12:29:11 +0100

    Update

---
+++ SecureForce Setup.py
@@ -0,0 +1,7 @@
+# Instalacja niezbędnych zależności
+npm install request-promise fs readline progress chalk
+
+# Konfiguracja programu
+echo "http://192.168.1.1" > httpLoginURL
+echo "users.txt" > userWordlist
+echo "passwords.txt" > passwordWordlist
